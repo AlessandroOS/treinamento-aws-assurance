cb3911e6a5d02bf5ba82814a1ff56c4f1212ead5
Alessandro De Oliveira <alessandro0014@hotmail.com>
2026-07-02 19:47:23 -0300

    First commit: Spring Boot project

---
+++ src/main/java/com/treinamento_aws/treinamento/aws/assurance/TreinamentoAwsAssuranceApplication.java
@@ -0,0 +1,13 @@
+package com.treinamento_aws.treinamento.aws.assurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinamentoAwsAssuranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinamentoAwsAssuranceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/treinamento_aws/treinamento/aws/assurance/TreinamentoAwsAssuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamento_aws.treinamento.aws.assurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinamentoAwsAssuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
