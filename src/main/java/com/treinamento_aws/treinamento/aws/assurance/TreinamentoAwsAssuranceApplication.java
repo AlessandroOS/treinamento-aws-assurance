1636fd194c2eccfdc9b221229fb7aa02bc99d09f
Alessandro De Oliveira <alessandro0014@hotmail.com>
2026-07-02 20:12:55 -0300

    adicionado spring web e endpoint de teste

--- src/main/java/com/treinamento_aws/treinamento/aws/assurance/TreinamentoAwsAssuranceApplication.java
+++ src/main/java/com/treinamento_aws/treinamento/aws/assurance/TreinamentoAwsAssuranceApplication.java
@@ -2,6 +2,7 @@ package com.treinamento_aws.treinamento.aws.assurance;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class TreinamentoAwsAssuranceApplication {
@@ -10,4 +11,8 @@ public class TreinamentoAwsAssuranceApplication {
 		SpringApplication.run(TreinamentoAwsAssuranceApplication.class, args);
 	}
 
+	@GetMapping("/teste")
+	public String meuEndpoint() {
+		return "Olá da instância EC2 da AWS! O endpoint funcionou com sucesso.";
+	}
 }
